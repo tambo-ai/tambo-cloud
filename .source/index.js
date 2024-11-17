be8032804bd35c5ede809627f27a6a0d02a15bbe
michaelmagan <michael.magan92@gmail.com>
2024-11-17 15:24:44 -0800

    remove generated files

--- .source/index.js
+++
@@ -1,4 +0,0 @@
-import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=67f18b943be9da2b2e4906613ad5682137dc0044e07dc11b9c52956db4f134f1"
-export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"/Users/michaelmagan/Documents/hydra-ai-landing/content/docs/index.mdx"})];
-export const meta = [];
--- .source/source.config.mjs
+++
@@ -1,9 +0,0 @@
-// source.config.ts
-import { defineDocs } from "fumadocs-mdx/config";
-var { docs, meta } = defineDocs({
-  dir: "content/docs"
-});
-export {
-  docs,
-  meta
-};
