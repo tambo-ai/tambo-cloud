f833eb4be7881a43bf68e056322873d2bf9cc8c5
Alec Flett <alecf@flett.org>
2025-01-13 16:14:12 -0800

    add hydra-ai-server to monorepo

---
+++ packages/hydra-ai-server/src/hydra-ai/services/llm/utils.ts
@@ -0,0 +1,10 @@
+
+import { ChatCompletionMessageParam } from "token.js";
+import { ChatMessage } from "../../model/chat-message";
+
+export function chatHistoryToParams(messageHistory: ChatMessage[]): ChatCompletionMessageParam[] {
+    return messageHistory.map((message) => ({
+        role: message.sender === "user" ? "user" : "system",
+        content: message.message,
+    }));
+}
