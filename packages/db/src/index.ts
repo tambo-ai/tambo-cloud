8717ccf796d9c857180c842f649d79009019a443
Alec Flett <alecf@flett.org>
2025-01-16 13:43:40 -0800

    do not use automatic snake casing

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,6 +5,6 @@ import * as schema from "./schema";
 // quick hack to get the db connection
 const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 
-const db = drizzle(client, { casing: "snake_case", schema });
+const db = drizzle(client, { schema });
 
 export { db, schema };
