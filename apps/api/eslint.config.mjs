9c7cfc45ffbd1dbb437e63d8ffec51b276ae8a3c
Alec Flett <alecf@flett.org>
2025-01-16 15:41:11 -0800

    lint fixes

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,4 +1,12 @@
 import { config } from '@use-hydra-ai/eslint-config/base';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default config;
+export default [
+  ...config,
+  {
+    rules: {
+      // Temporarily turning this off to reduce noise
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
