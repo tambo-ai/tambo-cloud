c6ff518992f4cdcf676eeb5ecb42cd62af161158
Alec Flett <alecf@flett.org>
2025-01-16 13:44:02 -0800

    example of querying for projects

--- apps/web/server/api/routers/user.ts
+++ apps/web/server/api/routers/user.ts
@@ -1,8 +1,23 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-
+import { schema } from "@use-hydra-ai/db";
 export const userRouter = createTRPCRouter({
   getUser: protectedProcedure.query(async ({ ctx }) => {
     const user = ctx.session.user;
     return user;
+  }),
+  getUserProjects: protectedProcedure.query(async ({ ctx }) => {
+    const userId = ctx.session.user.id;
+    const db = ctx.db;
+    const projects = await db.query.projects.findMany({
+      where: (projects, { eq, inArray }) =>
+        inArray(
+          projects.id,
+          db
+            .select({ id: schema.projectMembers.projectId })
+            .from(schema.projectMembers)
+            .where(eq(schema.projectMembers.userId, userId)),
+        ),
+    });
+    return projects;
   }),
 });
