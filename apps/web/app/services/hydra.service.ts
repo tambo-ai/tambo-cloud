1bb7d68eb5fecd845fbbb3dff4708ab630c8060f
Alec Flett <alecf@flett.org>
2025-01-17 17:28:59 -0800

    one by one comment out each service as I reimplement it

--- apps/web/app/services/hydra.service.ts
+++ apps/web/app/services/hydra.service.ts
@@ -1,6 +1,4 @@
 import { env } from "@/lib/env";
-import axios from "axios";
-import { UserDto } from "../types/user.dto";
 import { getSupabaseClient } from "../utils/supabase";
 
 const API_BASE_URL = env.NEXT_PUBLIC_HYDRA_API_URL || "https://api.usehydra.ai";
@@ -17,21 +15,21 @@ const getHeaders = async () => {
   };
 };
 
-export const createProject = async (projectName: string) => {
-  const response = await axios.post(
-    `${API_BASE_URL}/projects`,
-    { projectName },
-    { headers: await getHeaders() },
-  );
-  return response.data;
-};
+// export const createProject = async (projectName: string) => {
+//   const response = await axios.post(
+//     `${API_BASE_URL}/projects`,
+//     { projectName },
+//     { headers: await getHeaders() },
+//   );
+//   return response.data;
+// };
 
-export const getUserProjects = async () => {
-  const response = await axios.get(`${API_BASE_URL}/projects/user`, {
-    headers: await getHeaders(),
-  });
-  return response.data;
-};
+// export const getUserProjects = async () => {
+//   const response = await axios.get(`${API_BASE_URL}/projects/user`, {
+//     headers: await getHeaders(),
+//   });
+//   return response.data;
+// };
 
 // export const getProject = async (id: string) => {
 //   const response = await axios.get(`${API_BASE_URL}/projects/${id}`, {
@@ -40,50 +38,50 @@ export const getUserProjects = async () => {
 //   return response.data;
 // };
 
-export const removeProject = async (id: string) => {
-  const response = await axios.delete(`${API_BASE_URL}/projects/${id}`, {
-    headers: await getHeaders(),
-  });
-  return response.data;
-};
+// export const removeProject = async (id: string) => {
+//   const response = await axios.delete(`${API_BASE_URL}/projects/${id}`, {
+//     headers: await getHeaders(),
+//   });
+//   return response.data;
+// };
 
-export const generateApiKey = async (projectId: string, name: string) => {
-  const response = await axios.put(
-    `${API_BASE_URL}/projects/${projectId}/api-key/${name}`,
-    {},
-    { headers: await getHeaders() },
-  );
-  return response.data;
-};
+// export const generateApiKey = async (projectId: string, name: string) => {
+//   const response = await axios.put(
+//     `${API_BASE_URL}/projects/${projectId}/api-key/${name}`,
+//     {},
+//     { headers: await getHeaders() },
+//   );
+//   return response.data;
+// };
 
-export const getApiKeys = async (projectId: string) => {
-  const response = await axios.get(
-    `${API_BASE_URL}/projects/${projectId}/api-keys`,
-    { headers: await getHeaders() },
-  );
-  return response.data;
-};
+// export const getApiKeys = async (projectId: string) => {
+//   const response = await axios.get(
+//     `${API_BASE_URL}/projects/${projectId}/api-keys`,
+//     { headers: await getHeaders() },
+//   );
+//   return response.data;
+// };
 
-export const removeApiKey = async (projectId: string, apiKeyId: string) => {
-  const response = await axios.delete(
-    `${API_BASE_URL}/projects/${projectId}/api-key/${apiKeyId}`,
-    { headers: await getHeaders() },
-  );
-  return response.data;
-};
+// export const removeApiKey = async (projectId: string, apiKeyId: string) => {
+//   const response = await axios.delete(
+//     `${API_BASE_URL}/projects/${projectId}/api-key/${apiKeyId}`,
+//     { headers: await getHeaders() },
+//   );
+//   return response.data;
+// };
 
-export const addProviderKey = async (
-  projectId: string,
-  providerName: string,
-  providerKey: string,
-) => {
-  const response = await axios.put(
-    `${API_BASE_URL}/projects/${projectId}/provider-key`,
-    { providerName, providerKey },
-    { headers: await getHeaders() },
-  );
-  return response.data;
-};
+// export const addProviderKey = async (
+//   projectId: string,
+//   providerName: string,
+//   providerKey: string,
+// ) => {
+//   const response = await axios.put(
+//     `${API_BASE_URL}/projects/${projectId}/provider-key`,
+//     { providerName, providerKey },
+//     { headers: await getHeaders() },
+//   );
+//   return response.data;
+// };
 
 // export const removeProviderKey = async (
 //   projectId: string,
@@ -96,17 +94,17 @@ export const addProviderKey = async (
 //   return response.data;
 // };
 
-export const getProviderKeys = async (projectId: string) => {
-  const response = await axios.get(
-    `${API_BASE_URL}/projects/${projectId}/provider-keys`,
-    { headers: await getHeaders() },
-  );
-  return response.data;
-};
+// export const getProviderKeys = async (projectId: string) => {
+//   const response = await axios.get(
+//     `${API_BASE_URL}/projects/${projectId}/provider-keys`,
+//     { headers: await getHeaders() },
+//   );
+//   return response.data;
+// };
 
-export const createUser = async (userData: UserDto) => {
-  const response = await axios.post(`${API_BASE_URL}/users`, userData, {
-    headers: await getHeaders(),
-  });
-  return response.data;
-};
+// export const createUser = async (userData: UserDto) => {
+//   const response = await axios.post(`${API_BASE_URL}/users`, userData, {
+//     headers: await getHeaders(),
+//   });
+//   return response.data;
+// };
