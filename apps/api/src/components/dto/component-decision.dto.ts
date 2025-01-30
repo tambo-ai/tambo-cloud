386ebd6cb8597c8849052724224cb3e0eccd0bf7
Alec Flett <flett@usehydra.ai>
2025-01-30 10:50:27 -0800

    Expose more of the tool stuff as DTOs (#145)

--- apps/api/src/components/dto/component-decision.dto.ts
+++ apps/api/src/components/dto/component-decision.dto.ts
@@ -1,13 +1,28 @@
 import {
-  SuggestedAction,
-  ToolCallRequest,
+  SuggestedAction as SuggestedActionInterface,
+  ToolCallRequest as ToolCallRequestInterface,
 } from '@use-hydra-ai/hydra-ai-server';
 
-export class ComponentDecisionWithThreadId {
+export class ComponentDecision {
   componentName?: string | null;
   props?: Record<string, any>;
   message?: string;
   suggestedActions?: SuggestedAction[];
   toolCallRequest?: ToolCallRequest;
   threadId?: string;
+}
+
+export class ToolCallRequest implements Partial<ToolCallRequestInterface> {
+  toolCallId?: string;
+  tool?: string;
+  parameters?: {
+    parameterName: string;
+    parameterValue: any;
+  }[];
+  toolName?: string;
+}
+
+export class SuggestedAction implements Partial<SuggestedActionInterface> {
+  label?: string;
+  actionText?: string;
 }
