32399ae50b90159d9a5748619e5177b7c0f4a386
Alec Flett <flett@usehydra.ai>
2025-01-30 14:42:56 -0800

    Flush out tool parameter types as DTOs (#148)
    * add json-schema to api server for types
    
    * also flush out tool parameters

--- apps/api/src/components/dto/generate-component.dto.ts
+++ apps/api/src/components/dto/generate-component.dto.ts
@@ -1,7 +1,43 @@
 import {
-  AvailableComponents,
+  AvailableComponent as AvailableComponentInterface,
+  AvailableComponents as AvailableComponentsInterface,
   ChatMessage,
+  ComponentContextToolMetadata as ComponentContextToolMetadataInterface,
+  ComponentPropsMetadata as ComponentPropsMetadataInterface,
 } from '@use-hydra-ai/hydra-ai-server';
+import { JSONSchema7 } from 'json-schema';
+import { ComponentParameter } from './component-decision.dto';
+
+export class ComponentPropsMetadata
+  implements ComponentPropsMetadataInterface {}
+export class AvailableComponent implements AvailableComponentInterface {
+  name!: string;
+  description!: string;
+  parameters!: ComponentParameter[];
+  contextTools!: ComponentContextToolMetadata[];
+  props!: ComponentPropsMetadata;
+}
+
+export class AvailableComponents implements AvailableComponentsInterface {
+  [key: string]: AvailableComponent;
+}
+
+export class ToolParameters {
+  name!: string;
+  type!: string;
+  description!: string;
+  isRequired!: boolean;
+  items?: { type: string };
+  enumValues?: string[];
+  schema?: JSONSchema7;
+}
+export class ComponentContextToolMetadata
+  implements ComponentContextToolMetadataInterface
+{
+  name!: string;
+  description!: string;
+  parameters!: ToolParameters[];
+}
 
 export class GenerateComponentRequest {
   messageHistory?: ChatMessage[];
