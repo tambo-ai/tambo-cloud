e67bd71027898163560fd6a80cd61a12031fa4f0
Michael Milstead <mike.michaelmilstead@gmail.com>
2025-01-29 16:40:50 -0800

    include toolcallrequest in generate response (#142)

--- apps/api/src/components/dto/component-decision.dto.ts
+++ apps/api/src/components/dto/component-decision.dto.ts
@@ -1,9 +1,13 @@
-import { SuggestedAction } from '@use-hydra-ai/hydra-ai-server';
+import {
+  SuggestedAction,
+  ToolCallRequest,
+} from '@use-hydra-ai/hydra-ai-server';
 
 export class ComponentDecisionWithThreadId {
   componentName?: string | null;
   props?: Record<string, any>;
   message?: string;
   suggestedActions?: SuggestedAction[];
+  toolCallRequest?: ToolCallRequest;
   threadId?: string;
 }
