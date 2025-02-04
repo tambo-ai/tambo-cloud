6ce785014a57b518dcbd520fd97219bd858fce36
Alec Flett <alecf@flett.org>
2025-02-04 14:35:50 -0800

    show more thread details

--- apps/web/components/thread/thread-messages.tsx
+++ apps/web/components/thread/thread-messages.tsx
@@ -23,7 +23,15 @@ export function ThreadMessages({ thread }: Readonly<ThreadMessagesProps>) {
                 {new Date(message.createdAt).toLocaleString()}
               </p>
             </div>
-            <p className="whitespace-pre-wrap">{`${message.content}`}</p>
+            <div className="whitespace-pre-wrap">
+              {typeof message.content === "object" ? (
+                <pre className="max-h-[400px] max-w-full overflow-auto">
+                  {JSON.stringify(message.content, null, 2)}
+                </pre>
+              ) : (
+                `${message.content}`
+              )}
+            </div>
             {message.componentDecision && (
               <div className="mt-2 text-sm text-muted-foreground">
                 <div>
@@ -55,13 +63,18 @@ export function ThreadMessages({ thread }: Readonly<ThreadMessagesProps>) {
                 {message.componentDecision.toolCallRequest && (
                   <div className="mt-1">
                     Tool Call:{" "}
-                    {message.componentDecision.toolCallRequest.toolName}
+                    <pre>
+                      {message.componentDecision.toolCallRequest.toolName}
+                    </pre>
                     {message.componentDecision.toolCallRequest.parameters
                       .length > 0 && (
                       <ul className="list-disc list-inside">
                         {message.componentDecision.toolCallRequest.parameters.map(
                           (param, i) => (
-                            <li key={i}>{param.parameterName}</li>
+                            <li key={i}>
+                              {param.parameterName} ={" "}
+                              {JSON.stringify(param.parameterValue)}
+                            </li>
                           ),
                         )}
                       </ul>
