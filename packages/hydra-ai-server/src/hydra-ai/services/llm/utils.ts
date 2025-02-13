235711a3549634e3c3231cf9ba8e0eee45539b5e
Alec Flett <alecf@flett.org>
2025-02-13 15:13:33 -0800

    switch to map for readability

--- packages/hydra-ai-server/src/hydra-ai/services/llm/utils.ts
+++ packages/hydra-ai-server/src/hydra-ai/services/llm/utils.ts
@@ -4,18 +4,12 @@ import { ChatMessage } from "../../model/chat-message";
 export function chatHistoryToParams(
   messageHistory: ChatMessage[],
 ): ChatCompletionMessageParam[] {
-  const messages: ChatCompletionMessageParam[] = [];
-
-  messageHistory.forEach((message) => {
-    messages.push({
-      role: ["user", "tool"].includes(message.sender) ? "user" : "assistant",
-      content:
-        message.message +
-        (message.additionalContext
-          ? `<System> The following is additional context provided by the system that you can use when responding to the user:  ${message.additionalContext} </System>`
-          : ""),
-    });
-  });
-
-  return messages;
+  return messageHistory.map((message) => ({
+    role: ["user", "tool"].includes(message.sender) ? "user" : "assistant",
+    content:
+      message.message +
+      (message.additionalContext
+        ? `<System> The following is additional context provided by the system that you can use when responding to the user:  ${message.additionalContext} </System>`
+        : ""),
+  }));
 }
