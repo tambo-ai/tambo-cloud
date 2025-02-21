5ee03436bfe6ba5b374c8eee01e354c92c4f006c
Alec Flett <alecf@flett.org>
2025-02-20 18:10:30 -0800

    add provider for db here too

--- apps/api/src/components/components.module.ts
+++ apps/api/src/components/components.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { getDb } from '@use-hydra-ai/db';
 import { AIModule } from '../ai/ai.module';
 import { ProjectsModule } from '../projects/projects.module';
 import { ThreadsModule } from '../threads/threads.module';
 import { ComponentsController } from './components.controller';
 import { ComponentsService } from './components.service';
-
 @Module({
   imports: [ConfigModule, ProjectsModule, AIModule, ThreadsModule],
   controllers: [ComponentsController],
-  providers: [ComponentsService],
+  providers: [
+    ComponentsService,
+    {
+      provide: 'DbRepository',
+      useFactory: () => getDb(process.env.DATABASE_URL!),
+    },
+  ],
 })
 export class ComponentsModule {}
