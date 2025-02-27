e30dbbd1ed6c70977655a512104243cfcdc4a165
Alec Flett <alecf@flett.org>
2025-02-27 11:21:27 -0800

    fix typo

--- apps/api/src/threads/threads.module.ts
+++ apps/api/src/threads/threads.module.ts
@@ -17,7 +17,7 @@ import { ThreadsService } from './threads.service';
       useFactory: () => getDb(process.env.DATABASE_URL!),
     },
     {
-      provide: 'OPENAI_PI_KEY', //todo: weird api keys don't match?
+      provide: 'OPENAI_API_KEY', //todo: weird api keys don't match?
       useFactory: (configService: ConfigService) =>
         configService.get('OPENAI_API_KEY'),
       inject: [ConfigService],
