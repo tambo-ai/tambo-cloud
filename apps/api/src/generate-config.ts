3e2701023ce4c6078e00b78dc0adfb66329b8c86
Alec Flett <alecf@flett.org>
2025-02-28 11:52:33 -0800

    fix when no .env, e.g. in CI

--- apps/api/src/generate-config.ts
+++ apps/api/src/generate-config.ts
@@ -4,13 +4,16 @@ import { AppModule } from './app.module';
 import { generateOpenAPIConfig } from './common/openapi';
 
 async function generateConfig() {
+  // Add some dummy keys to the environment variables, so that the app can be started
+  process.env.OPENAI_API_KEY = 'DUMMY_KEY';
+  process.env.EXTRACTION_OPENAI_API_KEY = 'DUMMY_KEY';
   const app = await NestFactory.create(AppModule, {
     cors: true,
     logger: false,
   });
   const document = generateOpenAPIConfig(app);
   const filePath = process.env.OPENAPI_SPEC_FILE;
-  // now write the file to the file system in the location specified on the command line
+  // now write the file to the file system in the location specified by the OPENAPI_SPEC_FILE environment variable
 
   if (filePath) {
     fs.writeFileSync(filePath, JSON.stringify(document, null, 2));
@@ -20,4 +23,7 @@ async function generateConfig() {
   }
 }
 
-generateConfig();
+generateConfig().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
