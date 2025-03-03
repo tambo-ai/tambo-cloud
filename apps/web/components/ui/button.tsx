1c07fbd4fee04581ea9eee5b026279a0b71a8cb5
Michael Magan <magan@usehydra.ai>
2025-03-03 15:18:33 -0800

    update link to foreground

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-primary-foreground underline-offset-4 hover:underline",
 
         orange:
           "border border-input  hover:bg-accent hover:text-accent-foreground",
