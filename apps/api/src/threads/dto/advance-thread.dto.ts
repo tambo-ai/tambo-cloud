213cf2a8e2ea057f4474266229fff6e7f72b46cd
michael <mike.michaelmilstead@gmail.com>
2025-03-04 15:23:30 -0800

    update apischema decorators

--- apps/api/src/threads/dto/advance-thread.dto.ts
+++ apps/api/src/threads/dto/advance-thread.dto.ts
@@ -3,13 +3,14 @@ import { GenerationStage } from '@use-hydra-ai/core';
 import { AvailableComponentDto } from '../../components/dto/generate-component.dto';
 import { MessageRequest, ThreadMessageDto } from './message.dto';
 
+@ApiSchema({ name: 'AdvanceThread' })
 export class AdvanceThreadDto {
   messageToAppend?: MessageRequest;
   contextKey?: string;
   availableComponents?: AvailableComponentDto[];
 }
 
-@ApiSchema({ name: 'AdvanceThreadResponseDto' })
+@ApiSchema({ name: 'AdvanceThreadResponse' })
 export class AdvanceThreadResponseDto {
   responseMessageDto!: ThreadMessageDto;
   generationStage!: GenerationStage;
