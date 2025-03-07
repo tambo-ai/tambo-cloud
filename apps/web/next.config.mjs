33c6fd21d01b2d9be6e8bcfa2c027c67b9dfbc04
Alec Flett <flett@usehydra.ai>
2025-03-07 13:29:02 -0800

    fix: oops, this was local debugging (#461)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,21 +1,16 @@
 import { createMDX } from "fumadocs-mdx/next";
 import { createJiti } from "jiti";
-import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-
 const jiti = createJiti(fileURLToPath(import.meta.url));
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
 jiti.import("./lib/env");
 
 const withMDX = createMDX();
-console.log("setting outputFileTracingRoot", path.join(__dirname, "../../.."));
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  outputFileTracingRoot: path.join(__dirname, "../../.."),
-
   images: {
     remotePatterns: [
       {
