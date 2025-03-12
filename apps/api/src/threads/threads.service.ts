9fd023c3643d80b23b83ad132a938580aeb0924b
michael <mike.michaelmilstead@gmail.com>
2025-03-12 16:26:04 -0700

    add component info and state to llm message history only when needed

--- apps/api/src/threads/threads.service.ts
+++ apps/api/src/threads/threads.service.ts
@@ -717,23 +717,20 @@ function convertThreadMessagesToLegacyThreadMessages(
       sender: [MessageRole.User, MessageRole.Tool].includes(message.role)
         ? (message.role as 'user' | 'tool')
         : 'hydra',
-      message:
-        message.content
-          .map((part) => {
-            switch (part.type) {
-              case ContentPartType.Text:
-                return part.text ?? '';
-              default:
-                return '';
-            }
-          })
-          .join('') +
-        ' ' +
-        JSON.stringify({
-          component:
-            message.componentDecision ?? message.component ?? undefined,
-          componentState: message.componentState ?? undefined,
-        }),
+      message: message.content
+        .map((part) => {
+          switch (part.type) {
+            case ContentPartType.Text:
+              return part.text ?? '';
+            default:
+              return '';
+          }
+        })
+        .join(''),
+      componentDecision:
+        message.componentDecision?.componentName && message.componentDecision,
+      componentState: message.componentState,
+      actionType: message.actionType,
     }),
   );
 }
