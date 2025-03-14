b14ba0430584c5be45892468b5d3aee184737531
Alec Flett <alecf@flett.org>
2025-03-14 11:18:31 -0700

    make useSession behave like useQuery

--- apps/web/hooks/auth.ts
+++ apps/web/hooks/auth.ts
@@ -1,6 +1,6 @@
 import { getSupabaseClient } from "@/app/utils/supabase";
 import { Session } from "@supabase/supabase-js";
-import { useCallback, useEffect, useState } from "react";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
 enum SessionState {
   Uninitialized,
@@ -10,33 +10,19 @@ enum SessionState {
   Error,
 }
 
-export function useSession() {
-  const [session, setSession] = useState<Session | null>(null);
-  const [sessionState, setSessionState] = useState<SessionState>(
-    SessionState.Uninitialized,
-  );
-  const checkAuth = useCallback(async () => {
-    console.log("Checking auth status");
-    try {
+export function useSession(
+  queryOptions: Partial<UseQueryOptions<Session | null>> = {},
+) {
+  return useQuery({
+    queryKey: ["session"],
+    queryFn: async () => {
+      console.log("Checking auth status");
       const supabase = getSupabaseClient();
-      setSessionState(SessionState.Loading);
       const {
         data: { session },
       } = await supabase.auth.getSession();
-      setSession(session);
-      setSessionState(
-        session ? SessionState.Authenticated : SessionState.Unauthenticated,
-      );
-    } catch (error) {
-      console.error("Error checking auth status:", error);
-      setSession(null);
-      setSessionState(SessionState.Error);
-    }
-  }, []);
-
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-
-  return { session, sessionState };
+      return session;
+    },
+    ...queryOptions,
+  });
 }
