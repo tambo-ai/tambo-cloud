d200fdc6bb762ef62d55da9590dc52d6c5c6e119
Alec Flett <alecf@flett.org>
2025-03-14 16:11:10 -0700

    chore: add watch path to make npm link easier

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,6 @@
 import { createMDX } from "fumadocs-mdx/next";
 import { createJiti } from "jiti";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
@@ -11,6 +12,12 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  // This lets us use `npm link` and still get hot reloading - it allows
+  // ../../node_modules to be included in the list of watched files
+  outputFileTracingRoot: path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "../../",
+  ),
   images: {
     remotePatterns: [
       {
