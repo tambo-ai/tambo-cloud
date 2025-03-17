cc659a0cabc7c007cd80d6da03b44117ba02e103
Michael Magan <magan@usehydra.ai>
2025-03-17 16:10:05 -0700

    fix: generation of robots/sitemap

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -1,11 +1,7 @@
 import { MetadataRoute } from "next";
-import { headers } from "next/headers";
 
-export default async function robots(): Promise<MetadataRoute.Robots> {
-  const headersList = await headers();
-  const domain = headersList.get("host") as string;
-  const protocol = "https";
-  const baseUrl = `${protocol}://${domain}`;
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://localhost:3000";
 
   return {
     rules: {
