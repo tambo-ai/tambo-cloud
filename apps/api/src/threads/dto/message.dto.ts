b017299612f6efe5bd71f6932ed085373d3cfb7c
Alec Flett <alecf@flett.org>
2025-03-17 17:24:43 -0700

    start trying to use toolResponses from content instead of explicit toolResponse

--- apps/api/src/threads/dto/message.dto.ts
+++ apps/api/src/threads/dto/message.dto.ts
@@ -105,6 +105,9 @@ export class MessageRequest implements InternalThreadMessage {
   @IsEnum(ActionType)
   actionType?: ActionType;
 
+  /**
+   * @deprecated Put the response in the content instead
+   */
   @IsOptional()
   @ApiProperty({
     description: '@deprecated Put the response in the content instead',
