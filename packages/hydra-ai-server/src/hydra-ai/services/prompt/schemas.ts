9c32ac926728046b9b8f2f7b9a0e194c4352680c
Alec Flett <alecf@flett.org>
2025-03-27 15:38:42 -0700

    move props to the end so as not to confuse things

--- packages/hydra-ai-server/src/hydra-ai/services/prompt/schemas.ts
+++ packages/hydra-ai-server/src/hydra-ai/services/prompt/schemas.ts
@@ -7,12 +7,6 @@ const baseSchema = {
       "The message to be displayed to the user alongside the chosen component. Depending on the component type, and the user message, this message might include a description of why a given component was chosen, and what can be seen within it, or what it does.",
     ),
   componentName: z.string().describe("The name of the chosen component"),
-  props: z
-    .object({})
-    .passthrough()
-    .describe(
-      "The props that should be used in the chosen component. These will be injected by using React.createElement(component, props)",
-    ),
   reasoning: z.string().describe("The reasoning behind the decision"),
   componentState: z
     .record(z.any())
@@ -20,6 +14,11 @@ const baseSchema = {
     .describe(
       `Any additional state properties that should be injected into the component, 
 used to carry state forward from previous component decisions.`,
+    ),
+  props: z
+    .record(z.any())
+    .describe(
+      "The props that should be used in the chosen component. These will be injected by using React.createElement(component, props)",
     ),
 };
 
