1a5eb9bd31346fae87c2c713e7a03d34db390b57
Alec Flett <alecf@flett.org>
2025-04-03 14:50:50 -0700

    setup jest

---
+++ packages/hydra-ai-server/jest.config.ts
@@ -0,0 +1,20 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^@tambo-ai-cloud/(.*)$": "<rootDir>/../../packages/$1/src",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default config;
