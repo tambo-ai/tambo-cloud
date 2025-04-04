2f3055b046c7d3fe710ef8a588b18fa09f28b424
Alec Flett <alecf@flett.org>
2025-04-04 11:42:18 -0700

    fix tests to work

--- apps/api/src/components/components.controller.spec.ts
+++ apps/api/src/components/components.controller.spec.ts
@@ -1,14 +1,42 @@
+import { ConfigService } from "@nestjs/config";
 import { Test, TestingModule } from "@nestjs/testing";
+import { DATABASE } from "../common/middleware/db-transaction-middleware";
+import { CorrelationLoggerService } from "../common/services/logger.service";
+import { ProjectsService } from "../projects/projects.service";
+import { ThreadsService } from "../threads/threads.service";
 import { ComponentsController } from "./components.controller";
 import { ComponentsService } from "./components.service";
+
+// Always need to mock superjson because it is an ESM module
+jest.mock("superjson", () => ({
+  default: {
+    parse: jest.fn(),
+    stringify: jest.fn(),
+  },
+}));
 
 describe("ComponentsController", () => {
   let controller: ComponentsController;
 
   beforeEach(async () => {
+    const mockDb = {
+      // Add any database methods your services use
+      transaction: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ComponentsController],
-      providers: [ComponentsService],
+      providers: [
+        {
+          provide: DATABASE,
+          useValue: mockDb,
+        },
+        ConfigService,
+        ComponentsService,
+        ProjectsService,
+        ThreadsService,
+        CorrelationLoggerService,
+      ],
     }).compile();
 
     controller = module.get<ComponentsController>(ComponentsController);
