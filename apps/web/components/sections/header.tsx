283ab65c3a84b4cf0df1d82c3a2e0df8255ac2f3
Michael Magan <magan@usehydra.ai>
2025-04-09 19:52:30 -0700

    fix header alignment

--- apps/web/components/sections/header.tsx
+++ apps/web/components/sections/header.tsx
@@ -1,5 +1,6 @@
 import { Icons } from "@/components/icons";
 import { siteConfig } from "@/lib/config";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 // Client components
@@ -9,13 +10,20 @@ import { MobileNavigation } from "@/components/sections/mobile-navigation";
 export function Header({
   showDashboardButton = true,
   showLogoutButton = false,
+  className,
 }: {
   showDashboardButton?: boolean;
   showLogoutButton?: boolean;
+  className?: string;
 }) {
   return (
-    <header className="sticky top-0 h-[var(--header-height)] z-50 p-0 bg-background/60 backdrop-blur">
-      <div className="flex justify-between items-center container mx-auto p-2">
+    <header
+      className={cn(
+        "sticky top-0 h-[var(--header-height)] z-50 p-0 bg-background/60 backdrop-blur",
+        className,
+      )}
+    >
+      <div className="flex justify-between items-center mx-auto pt-2">
         <Link
           href="/"
           title="brand-logo"
