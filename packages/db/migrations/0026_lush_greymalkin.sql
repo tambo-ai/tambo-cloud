701b0066534e6d6e699e4da5666fea8cbd998b2b
Alec Flett <alecf@flett.org>
2025-04-10 09:57:14 -0700

    add migrations for bools

---
+++ packages/db/migrations/0026_lush_greymalkin.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "projects" ADD COLUMN "mcp_enabled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE "projects" ADD COLUMN "composio_enabled" boolean DEFAULT false NOT NULL;
