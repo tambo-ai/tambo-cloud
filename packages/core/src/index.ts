171c1f4fa1ff1446e1de5e4513ad89a7ac3433da
Alec Flett <alecf@flett.org>
2025-04-10 11:28:38 -0700

    add tools enum

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,4 +3,5 @@ export * from "./encrypt";
 export * from "./json";
 export * from "./templates";
 export * from "./threads";
+export * from "./tools";
 export type * from "./typeutils";
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -7,6 +7,7 @@ import {
   ChatCompletionContentPart,
   GenerationStage,
   MessageRole,
+  ToolProviderType,
 } from "@tambo-ai-cloud/core";
 import { relations, sql } from "drizzle-orm";
 import {
@@ -331,11 +332,6 @@ export const contacts = pgTable("contacts", ({ text, timestamp, uuid }) => ({
     .notNull(),
 }));
 
-enum ToolProviderType {
-  COMPOSIO = "composio",
-  MCP = "mcp",
-}
-
 export const toolProviders = pgTable(
   "tool_providers",
   ({ text, timestamp }) => ({
@@ -354,7 +350,7 @@ export const toolProviders = pgTable(
       .defaultNow()
       .notNull(),
     type: text("type", {
-      enum: Object.values<string>(ToolProviderType) as [ToolProviderType],
+      enum: Object.values(ToolProviderType) as [ToolProviderType],
     }).notNull(),
     url: text("url").notNull(),
     composio_app_name: text("composio_app_name"),
