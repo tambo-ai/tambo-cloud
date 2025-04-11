3ae26bacf8643e9fd9faafa67648cfd825093a5f
Alec Flett <alecf@flett.org>
2025-04-10 20:07:04 -0700

    oops

--- apps/web/components/dashboard-components/project-details/project-details-dialog.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Type definition for alert dialogs used across project details components
- */
-export type AlertState = {
-  show: boolean;
-  title: string;
-  description: string;
-  data?: { id: string; [key: string]: any };
-};
