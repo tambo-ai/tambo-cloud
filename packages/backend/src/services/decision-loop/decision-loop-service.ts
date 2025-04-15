3c69593ab36d6948b31275a652b4e396e1f08e95
michael <mike.michaelmilstead@gmail.com>
2025-04-15 13:59:43 -0700

    format decision response

--- packages/backend/src/services/decision-loop/decision-loop-service.ts
+++ packages/backend/src/services/decision-loop/decision-loop-service.ts
@@ -1,12 +1,13 @@
 import { objectTemplate } from "@libretto/openai";
 import {
   ChatCompletionMessageParam,
+  LegacyComponentDecision,
   ThreadMessage,
 } from "@tambo-ai-cloud/core";
 import { AvailableComponent } from "../../model/component-metadata";
 import { generateDecisionLoopPrompt } from "../../prompt/decision-loop-prompts";
 import { threadMessagesToChatHistory } from "../../util/threadMessagesToChatHistory";
-import { LLMClient } from "../llm/llm-client";
+import { getLLMResponseToolCallRequest, LLMClient } from "../llm/llm-client";
 import {
   convertComponentsToUITools,
   convertMetadataToTools,
@@ -39,7 +40,7 @@ export async function runDecisionLoop(
     },
   });
 
-  console.log(stream);
+  console.log("stream", stream);
   console.log(response);
 
   const toolCall = response.message?.tool_calls?.[0];
@@ -50,5 +51,26 @@ export async function runDecisionLoop(
     console.log(`Tool arguments: ${JSON.stringify(toolArgs)}`);
   }
 
-  return undefined;
+  const isUITool =
+    toolCall &&
+    componentTools.some(
+      (tool) => tool.function.name === toolCall.function.name,
+    );
+  const toolArgs = toolCall ? JSON.parse(toolCall.function.arguments) : {};
+
+  const decision: LegacyComponentDecision = {
+    reasoning: "",
+    message: response.message?.content || "",
+    componentName: isUITool ? toolCall?.function.name : "",
+    props: isUITool ? toolArgs : null,
+    componentState: null,
+    toolCallRequest:
+      !isUITool && toolCall
+        ? getLLMResponseToolCallRequest(response)
+        : undefined,
+  };
+
+  console.log("decision", decision);
+
+  return;
 }
