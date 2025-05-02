108d2c8ff500470c9ac7520fb405112cc50083d7
Alec Flett <flett@tambo.co>
2025-05-02 16:13:00 -0700

    expose mcp transport, store in db

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./ComponentDecision";
 export * from "./composio-utils";
 export * from "./encrypt";
 export * from "./json";
+export * from "./MCPClient";
 export * from "./templates";
 export * from "./threads";
 export * from "./tools";
