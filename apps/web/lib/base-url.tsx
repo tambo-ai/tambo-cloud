a8d0b632d00814ea644d694db8a1a6d5aa4e1095
Alec Flett <flett@tambo.co>
2025-05-30 17:56:50 -0700

    use getBaseUrl, and fall back to NEXT_PUBLIC_APP_URL

--- apps/web/lib/base-url.tsx
+++ apps/web/lib/base-url.tsx
@@ -4,6 +4,9 @@ export function getBaseUrl() {
   if (typeof window !== "undefined") {
     return window.location.origin;
   }
+  if (env.NEXT_PUBLIC_APP_URL) {
+    return env.NEXT_PUBLIC_APP_URL;
+  }
   if (env.VERCEL_URL) {
     return `https://${env.VERCEL_URL}`;
   }
