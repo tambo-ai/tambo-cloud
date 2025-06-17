0f5c807353784b134d21b942b152a6b212303c3a
Alec Flett <flett@tambo.co>
2025-06-17 15:22:51 -0700

    reorder declarations to make resolution work

--- apps/api/src/threads/dto/component-decision.dto.ts
+++ apps/api/src/threads/dto/component-decision.dto.ts
@@ -1,6 +1,29 @@
 import { ApiProperty, ApiSchema } from "@nestjs/swagger";
 import { ComponentDecisionV2, ToolCallRequest } from "@tambo-ai-cloud/core";
 
+export class ToolParameter {
+  parameterName!: string;
+  parameterValue!: any;
+}
+
+@ApiSchema({ name: "ToolCallRequest" })
+export class ToolCallRequestDto implements Partial<ToolCallRequest> {
+  /** @deprecated - The enclosing message's tool_call_id is used instead */
+  @ApiProperty({
+    deprecated: true,
+    description: "The unique id of the tool call, no longer used.",
+  })
+  tool_call_id?: string;
+  @ApiProperty({
+    description: "The parameters of the function to call.",
+  })
+  parameters!: ToolParameter[];
+  @ApiProperty({
+    description: "The name of the function to call.",
+  })
+  toolName!: string;
+}
+
 @ApiSchema({ name: "ComponentDecisionV2" })
 export class ComponentDecisionV2Dto implements ComponentDecisionV2 {
   componentName!: string | null;
@@ -33,27 +56,4 @@ export class ComponentDecisionV2Dto implements ComponentDecisionV2 {
       "The unique id of the tool call. This is filled in whether the tool call is a server-side or client-side tool call.",
   })
   toolCallId?: string;
-}
-
-export class ToolParameter {
-  parameterName!: string;
-  parameterValue!: any;
-}
-
-@ApiSchema({ name: "ToolCallRequest" })
-export class ToolCallRequestDto implements Partial<ToolCallRequest> {
-  /** @deprecated - The enclosing message's tool_call_id is used instead */
-  @ApiProperty({
-    deprecated: true,
-    description: "The unique id of the tool call, no longer used.",
-  })
-  tool_call_id?: string;
-  @ApiProperty({
-    description: "The parameters of the function to call.",
-  })
-  parameters!: ToolParameter[];
-  @ApiProperty({
-    description: "The name of the function to call.",
-  })
-  toolName!: string;
 }
