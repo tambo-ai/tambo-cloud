e640091df4c7669e59ce380a696a298e206a4261
Alec Flett <flett@tambo.co>
2025-06-25 12:24:38 -0700

    make middleware apply everywhere?

---
+++ apps/web/middleware.ts
@@ -0,0 +1,10 @@
+import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
+
+export default authkitMiddleware({
+  debug: true,
+});
+
+// Match against pages that require authentication
+// Leave this out if you want authentication on every page in your application
+// export const config = { matcher: ["/dashboard", "/"] };
+export const config = {};
