22984595d0f9da8ad7f7118fb74943bf84f4c3f8
Alec Flett <flett@tambo.co>
2025-06-25 19:23:04 -0700

    try comparing signin + auth urls

--- apps/web/app/workos/login/route.ts
+++ apps/web/app/workos/login/route.ts
@@ -1,11 +1,21 @@
-import { getSignInUrl } from "@workos-inc/authkit-nextjs";
+import { getSignInUrl, getWorkOS } from "@workos-inc/authkit-nextjs";
 import { redirect } from "next/navigation";
 
 export const GET = async () => {
+  const authorizationUrl = await getWorkOS().userManagement.getAuthorizationUrl(
+    {
+      provider: "authkit",
+      clientId: process.env.WORKOS_CLIENT_ID!,
+      redirectUri: "http://localhost:3000/workos/callback",
+    },
+  );
+  console.log("authorizationUrl", authorizationUrl);
   const signInUrl = await getSignInUrl({
-    organizationId: "org_01JYEX2XE7EKRVPVDTYP19YA90",
+    // provider: "authkit",
+    //     organizationId: "org_01JYEX2XE7EKRVPVDTYP19YA90",
   });
-  console.log("signInUrl", signInUrl);
+  console.log("signInUrl       ", signInUrl);
+  console.log("equal ", signInUrl === authorizationUrl);
 
-  return redirect(signInUrl);
+  return redirect(authorizationUrl);
 };
