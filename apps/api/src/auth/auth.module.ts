827d69e501205f7997824c58f4b705b5d1ff897d
Alec Flett <flett@tambo.co>
2025-06-27 15:37:51 -0700

    fix imports/dependencies

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { LoggerModule } from "../common/logger.module";
 import { ProjectsModule } from "../projects/projects.module";
 import { AuthController } from "./auth.controller";
 
 @Module({
-  imports: [LoggerModule, ProjectsModule],
+  imports: [ConfigModule, LoggerModule, ProjectsModule],
   controllers: [AuthController],
 })
 export class AuthModule {}
