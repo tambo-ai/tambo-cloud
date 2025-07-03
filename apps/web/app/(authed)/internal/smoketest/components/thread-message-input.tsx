2f1bf80c54c0cda5002ef7cb23e29ed869ebea56
Alec Flett <flett@tambo.co>
2025-07-02 17:17:47 -0700

    use auth token now

--- apps/web/app/(authed)/internal/smoketest/components/thread-message-input.tsx
+++ apps/web/app/(authed)/internal/smoketest/components/thread-message-input.tsx
@@ -4,7 +4,7 @@ import { useTamboThreadInput } from "@tambo-ai/react";
 import { FC, useState } from "react";
 
 interface ThreadMessageInputProps {
-  contextKey: string | undefined;
+  contextKey?: string;
   onSubmit: (value: string) => void;
 }
 
