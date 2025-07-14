37c479cb888a73787b479d94e193ebd6526b8491
Alec Flett <flett@tambo.co>
2025-07-14 15:33:55 -0700

    make this rule required

--- packages/eslint-config/next.mjs
+++ packages/eslint-config/next.mjs
@@ -47,6 +47,7 @@ export default tseslint.config(
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
+      "react-hooks/exhaustive-deps": "error",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
