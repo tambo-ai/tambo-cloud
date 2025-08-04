7362c6d17f3296049d43a8b0599a057d75a15d48
Michael Milstead <mike.michaelmilstead@gmail.com>
2025-08-04 13:46:55 -0700

    chore: test update thread fetch logic (#1474)

--- apps/api/src/threads/threads.service.ts
+++ apps/api/src/threads/threads.service.ts
@@ -1140,9 +1140,12 @@ export class ThreadsService {
       const currentTime = Date.now();
       if (currentTime - lastUpdateTime >= updateIntervalMs) {
         const start = Date.now();
-        const thread = await this.findOne(threadId, projectId);
-        const end = Date.now();
-        console.log(`findOne thread took ${end - start}ms`);
+        const thread = await operations.getThread(db, threadId, projectId);
+        if (!thread) {
+          throw new Error("Thread not found");
+        }
+        const threadFetchEnd = Date.now();
+
         if (thread.generationStage === GenerationStage.CANCELLED) {
           yield {
             responseMessageDto: {
@@ -1161,6 +1164,10 @@ export class ThreadsService {
           ...threadMessage,
           content: convertContentPartToDto(threadMessage.content),
         });
+        const updateMessageEnd = Date.now();
+        console.log(
+          `findOne thread took ${threadFetchEnd - start}ms, updateMessage took ${updateMessageEnd - threadFetchEnd}ms`,
+        );
         lastUpdateTime = currentTime;
       }
 
