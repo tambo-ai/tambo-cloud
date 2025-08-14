098bc24e1894b37b9afb900e36007d23b6d18863
akhileshrangani4 <akhileshrangani4@gmail.com>
2025-08-14 15:07:01 -0400

    chore: update Sentry trace sampling rate to 1 in both edge and server configurations and remove SENTRY_ENABLED env variable

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 1,
 
   // Enable logs to be sent to Sentry
   enableLogs: true,
