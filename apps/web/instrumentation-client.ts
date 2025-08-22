0ac886d3fd2f34decfbccac430c9ff71c71bcc92
Alec Flett <flett@tambo.co>
2025-08-22 12:03:02 -0700

    define sentry environments

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -3,9 +3,14 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+const environment =
+  process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ||
+  process.env.NODE_ENV ||
+  "development";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment,
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
