d8441c5490183916a440bf826bf059e22bb166da
Alec Flett <flett@tambo.co>
2025-08-22 16:34:25 -0700

    fix merge conflict

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -21,5 +21,4 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-  integrations: [Sentry.postgresIntegration()],
 });
