56d88316fb149efccd2c0b3a5e0f90bb643b8786
Alec Flett <flett@tambo.co>
2025-08-22 11:44:06 -0700

    no edge config, but include default integrations

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -15,5 +15,10 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-  integrations: [Sentry.postgresIntegration()],
+  integrations: (defaults) => [
+    ...defaults.filter(
+      (integration) => (integration as { name?: string }).name !== "Http",
+    ),
+    Sentry.postgresIntegration(),
+  ],
 });
