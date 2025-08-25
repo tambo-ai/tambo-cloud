89e0c938c290665e366959d3b441bff961965b12
Alec Flett <flett@tambo.co>
2025-08-25 10:23:40 -0700

    optional logger for easier testing

--- apps/api/src/threads/util/thread-state.ts
+++ apps/api/src/threads/util/thread-state.ts
@@ -364,9 +364,9 @@ export async function appendNewMessageToThread(
   db: HydraDb,
   threadId: string,
   newestMessageId: string,
-  logger: Logger,
   role: MessageRole = MessageRole.Assistant,
   initialText: string = "",
+  logger?: Logger,
 ) {
   try {
     const message = await db.transaction(
@@ -395,7 +395,7 @@ export async function appendNewMessageToThread(
 
     return message;
   } catch (error) {
-    logger.error(
+    logger?.error(
       "Transaction failed: Adding in-progress message",
       (error as Error).stack,
     );
