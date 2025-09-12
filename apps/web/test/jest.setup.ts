eb91db8af26dcd5dd33396c0d114d04e7fdf4679
Alec Flett <flett@tambo.co>
2025-09-12 15:39:54 -0700

    remove ts-expect-error

--- apps/web/test/jest.setup.ts
+++ apps/web/test/jest.setup.ts
@@ -6,15 +6,11 @@ class ResizeObserver {
   unobserve() {}
   disconnect() {}
 }
-// @ts-expect-error attach to global for jsdom
 if (!global.ResizeObserver) {
-  // @ts-expect-error jsdom global
   global.ResizeObserver = ResizeObserver;
 }
 
 // Polyfill scrollIntoView used by cmdk for option visibility
-// @ts-expect-error jsdom Element prototype
 if (!Element.prototype.scrollIntoView) {
-  // @ts-expect-error jsdom Element prototype assignment
   Element.prototype.scrollIntoView = function scrollIntoView() {};
 }
