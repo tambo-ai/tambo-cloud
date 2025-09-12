fac71a4d40292fd35c8d87a81314de26cc78ebdb
Alec Flett <flett@tambo.co>
2025-09-12 15:24:40 -0700

    combobox tests

--- apps/web/test/jest.setup.ts
+++ apps/web/test/jest.setup.ts
@@ -1,1 +1,20 @@
 import "@testing-library/jest-dom";
+
+// Polyfill ResizeObserver for components using cmdk/radix that rely on it
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+// @ts-expect-error attach to global for jsdom
+if (!global.ResizeObserver) {
+  // @ts-expect-error jsdom global
+  global.ResizeObserver = ResizeObserver;
+}
+
+// Polyfill scrollIntoView used by cmdk for option visibility
+// @ts-expect-error jsdom Element prototype
+if (!Element.prototype.scrollIntoView) {
+  // @ts-expect-error jsdom Element prototype assignment
+  Element.prototype.scrollIntoView = function scrollIntoView() {};
+}
