0fa16194262ec52d2c1ca6900c90106a99f1219a
Alec Flett <flett@tambo.co>
2025-10-01 15:56:41 -0700

    fix(db): Fix migration for extra constraint (#1861)

--- packages/db/migrations/0079_bizarre_princess_powerful.sql
+++ packages/db/migrations/0079_bizarre_princess_powerful.sql
@@ -1,3 +1,4 @@
+ALTER TABLE "mcp_thread_session" DROP CONSTRAINT IF EXISTS "mcp_thread_session_pk";
 ALTER TABLE "mcp_thread_session" DROP CONSTRAINT "mcp_thread_session_thread_id_tool_provider_id_idx";--> statement-breakpoint
 ALTER TABLE "mcp_thread_session" ALTER COLUMN "session_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "mcp_thread_session" ADD CONSTRAINT "mcp_thread_session_pk" PRIMARY KEY("thread_id","tool_provider_id");--> statement-breakpoint
