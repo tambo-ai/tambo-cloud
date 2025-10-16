a3d7d2765bb4e2db222e76c4b8f95c18631a405b
Alec Flett <flett@tambo.co>
2025-10-16 14:28:26 -0700

    first cut at the server

---
+++ apps/api/src/mcp-server/server.ts
@@ -0,0 +1,113 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { TAMBO_MCP_ACCESS_KEY_CLAIM } from "@tambo-ai-cloud/core";
+import { getDb, HydraDb } from "@tambo-ai-cloud/db";
+import { Express, Request, Response } from "express";
+import { randomUUID } from "node:crypto";
+import { getThreadMCPClients } from "src/common/systemTools";
+import { extractAndVerifyMcpAccessToken } from "../common/utils/oauth";
+
+export async function createMcpServer(
+  db: HydraDb,
+  projectId: string,
+  threadId: string,
+) {
+  const server = new McpServer(
+    {
+      name: "tambo-service",
+      version: "1.0.0",
+    },
+    {
+      capabilities: {
+        tools: {},
+      },
+      // Enable notification debouncing for specific methods
+      debouncedNotificationMethods: [
+        "notifications/tools/list_changed",
+        "notifications/resources/list_changed",
+        "notifications/prompts/list_changed",
+      ],
+    },
+  );
+
+  const mcpHandlers = {};
+  const mcpClients = await getThreadMCPClients(
+    db,
+    projectId,
+    threadId,
+    mcpHandlers,
+  );
+  const mapped = await Promise.allSettled(
+    mcpClients.map(async ({ client, serverId, url }) => {
+      console.log("Listing prompts for MCP server", serverId, url);
+      const promptResponse = await client.client.listPrompts();
+      for await (const prompt of promptResponse.prompts) {
+        await server.registerPrompt(
+          prompt.name,
+          {
+            title: prompt.title,
+            description: prompt.description,
+            argsSchema: prompt.inputSchema as Record<string, any>,
+          },
+          async (args) =>
+            await client.client.getPrompt({
+              name: prompt.name,
+              arguments: args,
+            }),
+        );
+      }
+    }),
+  );
+  for (const result of mapped) {
+    if (result.status === "rejected") {
+      console.error("Error listing prompts for MCP server", result.reason);
+    }
+  }
+  return server;
+}
+
+const handler = async (
+  req: Request,
+  res: Response,
+  projectId: string,
+  threadId: string,
+) => {
+  const db = getDb(process.env.DATABASE_URL!);
+  const server = await createMcpServer(db, projectId, threadId);
+  const transport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: () => randomUUID(),
+    enableJsonResponse: true,
+  });
+  await server.connect(transport);
+  await transport.handleRequest(req, res, req.body);
+};
+
+/**
+ * Register an express route that handles MCP requests for a given path
+ * @param expressApp - The express application to register the route on
+ * @param path - The path to register the route on
+ * @param server - The MCP server to handle the requests
+ */
+export function registerHandler(expressApp: Express, path: string) {
+  expressApp.use(path, async (req, res) => {
+    const authorization = req.header("authorization");
+    if (!authorization) {
+      res.status(401).send("Unauthorized");
+      return;
+    }
+
+    // make sure authorization is a bearer token
+    if (!authorization.toLowerCase().startsWith("bearer ")) {
+      res.status(401).send("Unauthorized");
+      return;
+    }
+    const [, bearerToken] = authorization.split(" ");
+    const payload = await extractAndVerifyMcpAccessToken(
+      bearerToken,
+      process.env.API_KEY_SECRET!,
+    );
+    const { projectId, threadId } = payload[TAMBO_MCP_ACCESS_KEY_CLAIM];
+
+    return await handler(req, res, projectId, threadId);
+  });
+}
